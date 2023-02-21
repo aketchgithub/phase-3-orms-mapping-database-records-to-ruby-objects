8323265f984601058a48e258e0c60d480459f25d
Victor Aketch <vickeryace@gmail.com>
2023-02-21 08:43:01 +0300

    Completed task

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,27 @@ class Song
     song.save
   end
 
+  def self.all
+    sql = <<-SQL
+    SELECT * FROM songs
+    SQL
+
+    DB[:conn].query(query, sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM songs
+    WHERE name = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].query(query, sql, name).map do |row|
+      self.new_from_db(row)
+      end.first
+    end
+
+
 end
